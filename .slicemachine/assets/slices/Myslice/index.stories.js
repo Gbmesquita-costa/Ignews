7e5d9bf37960b64c2ed07907f0cfaaba1cbbc3e3
Gabriel <mesquitag313@gmail.com>
2022-06-22 20:28:16 -0300

    Ignews

--- .slicemachine/assets/slices/Myslice/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/Myslice';
-
-export default {
-  title: 'slices/Myslice'
-}
-
-
-export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"myslice","items":[],"primary":{"title":[{"type":"heading1","text":"Synthesize leading-edge communities","spans":[]}],"description":[{"type":"paragraph","text":"Amet pariatur duis occaecat.","spans":[]}]},"id":"_Default"}} />
-_Default.storyName = 'Default'
